4312e7adfbbd1a5069442f432adf3dd66e9f4032
Kevin Ernest Long <kevinelong@gmail.com>
2025-05-19 16:03:22 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,42 @@
+
+//The -able suffix.
+// recycable - recycle()
+// edible - eat()
+// flippable - flip()
+
 //todo items are DoAble
 interface IDoable{
     public void done();
 }
 
-public class Task implements IDoable {
+interface IFlappable{
+    public void flap();
+}
+
+class Task implements IDoable, IFlappable {
 
     @Override
     public void done() {
+        System.out.println("Its marked as complete!!!");
+    }
 
+    @Override
+    public void flap() {
+        System.out.println("Whoosh whoosh!!!");
+    }
+}
+
+class BigTask extends Task{
+    public void done() {
+        System.out.println("Its marked as complete!!! Whooo!!!");
+    }
+}
+
+class Main{
+
+    public static void main(String[] args) {
+        BigTask t = new BigTask();
+        t.done();
+        t.flap();
     }
 }
